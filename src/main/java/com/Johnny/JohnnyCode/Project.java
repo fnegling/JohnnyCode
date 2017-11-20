6c9710f3e4c3780b37f2d77286e5a06d1187c5a5
o6msqx-Johnny <jiang.li@siemens.com>
2017-11-20 16:32:57 +0800

    Web

--- src/main/java/com/Johnny/JohnnyCode/Project.java
+++ src/main/java/com/Johnny/JohnnyCode/Project.java
@@ -1,40 +1,40 @@
-package com.Johnny.JohnnyCode;
-
-public class Project {
-    private  String Projectname;
-    private  String name;
-    private  String projectId;
-    private  String task;
-
-    public String getProjectname() {
-        return Projectname;
-    }
-
-    public void setProjectname(String project) {
-        Projectname = project;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getProjectId() {
-        return projectId;
-    }
-
-    public void setProjectId(String projectId) {
-        this.projectId = projectId;
-    }
-
-    public String getTask() {
-        return task;
-    }
-
-    public void setTask(String task) {
-        this.task = task;
-    }
-}
+//package com.Johnny.JohnnyCode;
+//
+//public class Project {
+//    private  String Projectname;
+//    private  String name;
+//    private  String projectId;
+//    private  String task;
+//
+//    public String getProjectname() {
+//        return Projectname;
+//    }
+//
+//    public void setProjectname(String project) {
+//        Projectname = project;
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public String getProjectId() {
+//        return projectId;
+//    }
+//
+//    public void setProjectId(String projectId) {
+//        this.projectId = projectId;
+//    }
+//
+//    public String getTask() {
+//        return task;
+//    }
+//
+//    public void setTask(String task) {
+//        this.task = task;
+//    }
+//}
--- src/main/java/com/Johnny/JohnnyCode/ProjectListRepository.java
+++ src/main/java/com/Johnny/JohnnyCode/ProjectListRepository.java
@@ -1,10 +1,10 @@
-package com.Johnny.JohnnyCode;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
-
-
-public interface ProjectListRepository extends JpaRepository<Project, Long> {
-    List<Project> findByProject(String name);
-}
+//package com.Johnny.JohnnyCode;
+//
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//import java.util.List;
+//
+//
+//public interface ProjectListRepository extends JpaRepository<Project, Long> {
+//    List<Project> findByProject(String name);
+//}
